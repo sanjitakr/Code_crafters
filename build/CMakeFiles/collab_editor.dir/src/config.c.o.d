2616f3b5d2cf928c758b230783e48968b7f19dfc
Sanjita Kumar <101909282+sanjitakr@users.noreply.github.com>
2025-10-12 02:44:48 +0530

    Delete build directory

--- build/CMakeFiles/collab_editor.dir/src/config.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/collab_editor.dir/src/config.c.o: \
- /home/skumar/Documents/Code_crafters/src/config.c \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/collab_editor.dir/src/editor.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/collab_editor.dir/src/editor.c.o: \
- /home/skumar/Documents/Code_crafters/src/editor.c \
- /usr/include/stdc-predef.h
